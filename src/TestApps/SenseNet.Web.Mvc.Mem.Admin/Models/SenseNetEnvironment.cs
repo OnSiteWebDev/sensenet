8fa780535855e0e4ffd0bc9abb70755cd2872847
Zoltán Gyebrovszki <kavics@sensenet.com>
2020-02-21 09:41:00 +0100

    Fix environment

--- src/TestApps/SenseNet.Web.Mvc.Mem.Admin/Models/SenseNetEnvironment.cs
+++ src/TestApps/SenseNet.Web.Mvc.Mem.Admin/Models/SenseNetEnvironment.cs
@@ -8,10 +8,14 @@ namespace SenseNet.Web.Mvc.Mem.Admin.Models
     public class ContainerEnvironment
     {
         public string Name { get; set; }
+    }
+    public class AuthenticationEnvironment
+    {
         public string Authority { get; set; }
     }
     public class SenseNetEnvironment
     {
         public ContainerEnvironment Container { get; set; }
+        public AuthenticationEnvironment Authentication { get; set; }
     }
 }
